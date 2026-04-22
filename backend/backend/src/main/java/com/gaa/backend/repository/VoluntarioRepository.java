061be1807a8d7ad69a823e83d719deb6dd3dba52
Maria Gabriela Martins <mgabrielamnts@gmail.com>
2026-04-22 16:33:29 -0300

    Delete backend/backend directory

--- backend/backend/src/main/java/com/gaa/backend/repository/VoluntarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.gaa.backend.repository;
-
-import com.gaa.backend.model.Voluntario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VoluntarioRepository extends JpaRepository<Voluntario, Long> {
-    boolean existsByCpf(String cpf);
-}
