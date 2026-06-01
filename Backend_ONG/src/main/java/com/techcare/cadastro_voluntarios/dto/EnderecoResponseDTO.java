d374cbe6a8a86d4cb02ea9b12f7b608ba92e7ef3
Maria Gabriela Martins <mgabrielamnts@gmail.com>
2026-06-01 12:26:20 -0300

    Atualizações back v2

---
+++ Backend_ONG/src/main/java/com/techcare/cadastro_voluntarios/dto/EnderecoResponseDTO.java
@@ -0,0 +1,43 @@
+package com.gaa.backend.dto.response;
+
+import com.gaa.backend.enums.Estado;
+import lombok.Getter;
+import lombok.Setter;
+
+public class EnderecoResponseDTO {
+
+    @Getter
+    @Setter
+    private Long id;
+
+    @Getter
+    @Setter
+    private String cep;
+
+    @Getter
+    @Setter
+    private String cidade;
+
+    @Getter
+    @Setter
+    private String bairro;
+
+    @Getter
+    @Setter
+    private String logradouro;
+
+    @Getter
+    @Setter
+    private String numero;
+
+    @Getter
+    @Setter
+    private String complemento;
+
+    @Getter
+    @Setter
+    private Estado estado;
+
+    public EnderecoResponseDTO() {
+    }
+}
---
+++ Backend_ONG/src/main/java/com/techcare/cadastro_voluntarios/dto/VoluntarioRequestDTO.java
@@ -0,0 +1,54 @@
+package com.gaa.backend.dto.request;
+
+import com.gaa.backend.enums.StatusVoluntario;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+public class VoluntarioRequestDTO {
+
+    @Getter
+    @Setter
+    private String nome;
+
+    @Getter
+    @Setter
+    private String cpf;
+
+    @Getter
+    @Setter
+    private String profissao;
+
+    @Getter
+    @Setter
+    private String registroConselho;
+
+    @Getter
+    @Setter
+    private Integer horasSemanaisDisponiveis;
+
+    @Getter
+    @Setter
+    private StatusVoluntario status;
+
+    @Getter
+    @Setter
+    private List<Long> areasAtuacaoIds;
+
+    @Getter
+    @Setter
+    private List<ContatoRequestDTO> contatos;
+
+    @Getter
+    @Setter
+    private List<EnderecoRequestDTO> enderecos;
+
+    @Getter
+    @Setter
+    private List<DisponibilidadeRequestDTO> disponibilidades;
+
+    public VoluntarioRequestDTO() {
+    }
+
+}
