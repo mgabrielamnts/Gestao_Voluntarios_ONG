0972dd2836ee23d30403182f8d76a80fb65bae84
Maria Gabriela Martins <mgabrielamnts@gmail.com>
2026-06-01 12:30:00 -0300

    Delete Backend_ONG/src/main/java/com/techcare/cadastro_voluntarios/exception/ResourceNotFoundException.java

--- Backend_ONG/src/main/java/com/techcare/cadastro_voluntarios/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.techcare.cadastro_voluntarios.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
